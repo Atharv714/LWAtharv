c8d6c78d839c8dc181df40eff297c2c3b10ce70b
Atharv714 <75135606+Atharv714@users.noreply.github.com>
2022-05-28 23:10:51 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,43 @@
+const colors = require('tailwindcss/colors')
+const plugin = require('tailwindcss/plugin')
+
+module.exports = {
+  content: ["./*html"],
+  theme: {
+    colors : 
+      {
+        "cblue": "#0000ff",
+        'realcyan': "#00ffff"
+      },
+
+      fonts:
+      {
+
+      },
+    extend: {
+      
+    },
+
+    screens: {
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+    }
+  },
+  plugins: [
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/forms'),
+    
+  ],
+}
